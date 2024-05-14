0d5a442e7f80b31f6fee92573a756a2be880438d
Avelin Ann <avelinannvarghese@gmail.com>
2024-05-14 23:24:37 +0530

    3D models

--- src/components/ComputerCanvas.jsx
+++ src/components/ComputerCanvas.jsx
@@ -4,19 +4,12 @@ import { OrbitControls, Preload, useGLTF, Html } from "@react-three/drei";
 import CanvasLoader from "./CanvasLoader";
 
 const ComputerCanvas = () => {
-  //ismobile changes at 1.05 =>add if needed
-  const [isMobile, setIsMobile] = useState(false);
   const computer = useGLTF("./pc_3d/scene.gltf");
   useEffect(() => {});
   return (
     <mesh>
       <hemisphereLight intensity={0.15} groundColor={"black"} />
-      <primitive
-        object={computer.scene}
-        //scale={}
-        position={[0, 1, -5.5]}
-        //rotation={[-0.01, -0.6, -0.1]}
-      />
+      <primitive object={computer.scene} position={[2, 4.5, -1.5]} />
     </mesh>
   );
 };
@@ -25,7 +18,7 @@ const ComputerModel = () => {
     <Canvas
       frameloop="demand"
       shadows
-      camera={{ position: [20, 30, 5], fov: 25 }}
+      camera={{ position: [36, 30, 1], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
     >
       <Suspense fallback={<CanvasLoader />}>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../style";
-// import ComputerCanvas from "./ComputerCanvas";
 import ComputerModel from "./ComputerCanvas";
 
 const Hero = () => {
@@ -9,20 +8,16 @@ const Hero = () => {
     <section className="w-full h-screen mx-auto">
       <div className="absolute inset-0 top-20">
         <div className="flex ">
-          <div className="text-6xl p-8 mt-[100px] w-[50%]">
-            <h1 className={styles.paddingX}>
-              Hi, I am <span className="text-[#AA3A6A] text-7xl"> Avelin</span>
+          <div className="p-8 mt-[100px]">
+            <h1 className={`${styles.paddingX + styles.mainHeadText}`}>
+              👋 Hello there! I'm
+              <span className="text-[#AA3A6A]"> Avelin</span>
             </h1>
-            <p className={styles.mainSubText + styles.paddingX}>
-              I love building awesome web pages.
-            </p>
           </div>
-
-          {/* <div className="w-full h-[60vw]">
-            <ComputerModel />
-          </div> */}
         </div>
-        <ComputerModel />
+        <div className="h-[100vw]">
+          <ComputerModel />
+        </div>
       </div>
     </section>
   );
